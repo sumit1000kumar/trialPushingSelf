bbc6f727eeb71bc66a82240258919983f4bbf93b
sumit1000kumar <sumitkumar9012004@gmail.com>
2025-01-17 21:58:47 +0530

    try

---
+++ functions.js
@@ -0,0 +1,14 @@
+function degreeToFarenheit (degreeCelcius){
+    let farenheit = (degreeCelcius*9/5)+32;
+    return farenheit;
+}
+
+console.log("The conversion of degree celcius in farenheit is "+ degreeToFarenheit(37));
+
+// HTTP response codes
+
+// 1 I am still wokring  
+// 2 All ok
+// 3 Get Away
+// 4 You messed up 
+// 5 I messed up
